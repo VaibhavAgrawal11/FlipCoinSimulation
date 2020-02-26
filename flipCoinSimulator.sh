6102e2cbaa9f50bde542eec7cbe6145e2513b8bb
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-26 14:43:38 +0530

    MODIFIED: Storing three flips in dictionary and determining percentage of triplet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
-printf "Storing two flips in dictionary and determining percentage of doublet combination\n"
+printf "Storing three flips in dictionary and determining percentage of triplet combination\n"
+
 #CONSTANTS
 HEADS=1
 TAILS=0
+
 #VARIABLES
+trippleCount=0
+hhhCount=0
+hhtCount=0
+hthCount=0
+thhCount=0
+tttCount=0
+tthCount=0
+thtCount=0
+httCount=0
 doubleCount=0
 count=0
 value=""
@@ -14,10 +25,13 @@ hhCount=0
 ttCount=0
 htCount=0
 thCount=0
-printf "Enter number of times you want to flip the coin: \n"
-read iteration
 headCount=0
 tailCount=0
+printf "Enter number of times you want to flip the coin: \n"
+read iteration
+printf "Enter 1:single flip\n2:double flip\n3:tripple flip\n"
+read choice
+
 #DICTIONARIES
 declare -A singletCount
 
@@ -34,49 +48,112 @@ function flipCoin()
 	echo $value
 }
 #FLIP THE COIN TILL THE ITERATION IS EQUAL TO ZERO
-while(($iteration>0))
-do	
-	arrayCount=0
-	value=$( flipCoin ) #STORING VALUE OF FIRST COIN
-	array1[$arrayCount]=$value #STORING VALUE OF FIRDT COIN IN SECOND COIN
-	arrayCount=$((arrayCount+1))
-	value2=$( flipCoin ) #STORING VALUE OF SECOND COIN
-	array1[$arrayCount]=$value2
-	iteration=$((iteration-1))
-	count=$((count+1))
-	singletCount["$count"]="$value"
-	doubletCount["$count"]="${array1[@]}"
-done
-echo ${singletCount[@]}
-echo ${doubletCount[@]}
+case $choice in
+	1)
+	while(($iteration>0))
+	do	
+		value=$( flipCoin ) #STORING VALUE OF FIRST COIN
+		iteration=$((iteration-1))
+		count=$((count+1))
+		singletCount["$count"]="$value"
+	done
+	echo ${singletCount[@]}
+	#COUNTING SINGLET HEADS AND TAILS
+	for value in ${singletCount[@]}
+	do
+   	if [ "$value" = "H" ]; then
+      	headCount=$((headCount+1))
+   	else
+      	tailCount=$((tailCount+1))
+   	fi
+	done
+	headPercent=`echo "scale=3; $headCount/$count*100" | bc -l`
+	tailPercent=`echo "scale=3; $tailCount/$count*100" | bc -l`
+	;;
 
-#COUNTING SINGLET HEADS AND TAILS
-for value in ${singletCount[@]}
-do
-	if [ "$value" = "H" ]; then
-		headCount=$((headCount+1))
-	else
-		tailCount=$((tailCount+1))
-	fi
-done
-headPercent=`echo "scale=3; $headCount/$count*100" | bc -l`
-tailPercent=`echo "scale=3; $tailCount/$count*100" | bc -l`
+	2)
+	while(($iteration>0))
+   do 
+      arrayCount=0
+      value=$( flipCoin ) 
+      array1[$arrayCount]=$value #STORING VALUE IN SECOND COIN
+      arrayCount=$((arrayCount+1))
+      value2=$( flipCoin ) #STORING VALUE OF SECOND COIN
+      array1[$arrayCount]=$value2
+      iteration=$((iteration-1))
+      count=$((count+1))
+      doubletCount["$count"]="${array1[@]}"
+   done
+   echo ${doubletCount[@]}
+	#COUNTING DOUBLET HEADS AND TAILS
+	for ((value=1; value<=${#doubletCount[@]}; value++))
+	do
+   	if [ "${doubletCount[$value]}" = "H T" ]; then
+      	htCount=$((htCount+1))
+   	elif [ "${doubletCount[$value]}" = "T H" ]; then
+      	thCount=$((thCount+1))
+   	elif [ "${doubletCount[$value]}" = "T T" ]; then
+      	ttCount=$((ttCount+1))
+   	else 
+      	hhCount=$((hhCount+1))
+   	fi
+   	doubleCount=$((doubleCount+1))
+	done
+	hhPercent=`echo "scale=3; $hhCount/$doubleCount*100" | bc -l`
+	htPercent=`echo "scale=3; $htCount/$doubleCount*100" | bc -l`
+	ttPercent=`echo "scale=3; $ttCount/$doubleCount*100" | bc -l`
+	thPercent=`echo "scale=3; $thCount/$doubleCount*100" | bc -l`
+	;;
 
-#COUNTING DOUBLET HEADS AND TAILS
-for ((value=1; value<=${#doubletCount[@]}; value++))
-do
-	if [ "${doubletCount[$value]}" = "H T" ]; then
-		htCount=$((htCount+1))
-	elif [ "${doubletCount[$value]}" = "T H" ]; then
-      thCount=$((thCount+1))
-	elif [ "${doubletCount[$value]}" = "T T" ]; then
-      ttCount=$((ttCount+1))
-	else 
-		hhCount=$((hhCount+1))
-	fi
-	doubleCount=$((doubleCount+1))
-done
-hhPercent=`echo "scale=3; $hhCount/$doubleCount*100" | bc -l`
-htPercent=`echo "scale=3; $htCount/$doubleCount*100" | bc -l`
-ttPercent=`echo "scale=3; $ttCount/$doubleCount*100" | bc -l`
-thPercent=`echo "scale=3; $thCount/$doubleCount*100" | bc -l`
+	3)
+   while(($iteration>0))
+   do 
+      arrayCount=0
+      value=$( flipCoin ) 
+      array1[$arrayCount]=$value #STORING VALUE IN SECOND COIN
+      arrayCount=$((arrayCount+1))
+      value2=$( flipCoin ) #STORING VALUE OF SECOND COIN
+      array1[$arrayCount]=$value2
+		arrayCount=$((arrayCount+1))
+		value3=$( flipCoin ) #STORING VALUE OF THIRD COIN
+      array1[$arrayCount]=$value3
+      iteration=$((iteration-1))
+      count=$((count+1))
+      tripletCount["$count"]="${array1[@]}"
+   done
+	echo ${tripletCount[@]}
+	for ((value=1; value<=${#tripletCount[@]}; value++))
+   do
+      if [ "${tripletCount[$value]}" = "H H H" ]; then
+         hhhCount=$((hhhCount+1))
+      elif [ "${tripletCount[$value]}" = "H H T" ]; then
+         hhtCount=$((hhtCount+1))
+      elif [ "${tripletCount[$value]}" = "H T H" ]; then
+         hthCount=$((hthCount+1))
+		elif [ "${tripletCount[$value]}" = "T H H" ]; then
+         thhCount=$((thhCount+1))
+      elif [ "${tripletCount[$value]}" = "T T H" ]; then
+         tthCount=$((tthCount+1))
+      elif [ "${tripletCount[$value]}" = "T H T" ]; then
+         thtCount=$((thtCount+1))
+      elif [ "${tripletCount[$value]}" = "H T T" ]; then
+         httCount=$((httCount+1))
+      elif [ "${tripletCount[$value]}" = "T T T" ]; then
+         tttCount=$((tttCount+1))
+      fi
+      trippleCount=$((trippleCount+1))
+   done
+   hhhPercent=`echo "scale=3; $hhhCount/$trippleCount*100" | bc -l`
+	hhtPercent=`echo "scale=3; $hhtCount/$trippleCount*100" | bc -l`
+	hthPercent=`echo "scale=3; $hthCount/$trippleCount*100" | bc -l`
+	thhPercent=`echo "scale=3; $thhCount/$trippleCount*100" | bc -l`
+	tttPercent=`echo "scale=3; $tttCount/$trippleCount*100" | bc -l`
+	tttPercent=`echo "scale=3; $tttCount/$trippleCount*100" | bc -l`
+	tthPercent=`echo "scale=3; $tthCount/$trippleCount*100" | bc -l`
+	thtPercent=`echo "scale=3; $thtCount/$trippleCount*100" | bc -l`
+	httPercent=`echo "scale=3; $httCount/$trippleCount*100" | bc -l`
+	;;
+	
+	*)
+	printf "Invalid input..\n"
+esac
